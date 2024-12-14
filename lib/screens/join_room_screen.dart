8032e8e731845f19df1672d5bf4f36126ba9cdc0
Parth Darji <parthdarji2961@gmail.com>
2024-12-14 15:06:10 +0530

    join room screen ui completed

--- lib/screens/join_room_screen.dart
+++ lib/screens/join_room_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../responsive/responsive.dart';
+import '../widgets/custom_button.dart';
+import '../widgets/custom_text.dart';
+import '../widgets/custom_textfield.dart';
+
 class JoinRoomScreen extends StatefulWidget {
   static String routeName = "join-room";
   const JoinRoomScreen({super.key});
@@ -9,8 +14,56 @@ class JoinRoomScreen extends StatefulWidget {
 }
 
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final _nameController = TextEditingController();
+  final _gameIdController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _gameIdController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 40,
+                    color: Colors.deepPurple,
+                  ),
+                ],
+                text: "Join Room",
+                fontSize: 70,
+              ),
+              SizedBox(height: size.height * 0.08),
+              CustomTextfield(
+                controller: _nameController,
+                hintText: "Enter your nickname",
+              ),
+              SizedBox(height: 20),
+              CustomTextfield(
+                controller: _gameIdController,
+                hintText: "Enter Game ID",
+              ),
+              SizedBox(height: size.height * 0.045),
+              CustomButton(
+                onPressed: () {},
+                buttonText: "Join",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
